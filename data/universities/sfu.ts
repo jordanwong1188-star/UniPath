812e3a4bdacbcba9bec87e14abf547a7b61643ba
jordanwong1188-star <jordan.wong1188@gmail.com>
2026-08-09 21:46:29 +0000

    Add UniPath school database

---
+++ data/universities/sfu.ts
@@ -0,0 +1,37 @@
+import { University } from "@/types/university";
+
+const sfu: University = {
+  id: "sfu",
+
+  name: "Simon Fraser University",
+
+  shortName: "SFU",
+
+  logo: "/logos/sfu.png",
+
+  province: "British Columbia",
+
+  city: "Burnaby",
+
+  type: "Public",
+
+  website: "https://www.sfu.ca",
+
+  admissionsWebsite: "https://www.sfu.ca/admission",
+
+  admissionAverage: "Mid to high 80s",
+
+  tuitionDomestic: "$6,000-$8,000/year",
+
+  applicationDeadline: "January 15",
+
+  studentPopulation: 35000,
+
+  residence: true,
+
+  coop: true,
+
+  faculties: []
+};
+
+export default sfu;
---
+++ data/universities/uoft.ts
@@ -0,0 +1,48 @@
+import { University } from "@/types/university";
+
+const uoft: University = {
+  id: "uoft",
+
+  name: "University of Toronto",
+
+  shortName: "UofT",
+
+  logo: "/logos/uoft.png",
+
+  province: "Ontario",
+
+  city: "Toronto",
+
+  type: "Public",
+
+  website: "https://www.utoronto.ca",
+
+  admissionsWebsite: "https://future.utoronto.ca",
+
+  admissionAverage: "High 80s to 90s",
+
+  tuitionDomestic: "$6,000-$8,000/year",
+
+  applicationDeadline: "January 15",
+
+  studentPopulation: 97000,
+
+  residence: true,
+
+  coop: true,
+
+  faculties: [
+    {
+      name: "Rotman Commerce",
+
+      programs: [
+        {
+          name: "Bachelor of Commerce",
+          degree: "BCom"
+        }
+      ]
+    }
+  ]
+};
+
+export default uoft;
