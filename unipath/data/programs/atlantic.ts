9f11a8d02355a36d6442036f04fcec62f2961235
jordanwong1188-star <jordan.wong1188@gmail.com>
2026-08-18 20:48:52 -0700

    Aggregate comprehensive Atlantic Canada catalogs

---
+++ unipath/data/programs/atlantic.ts
@@ -0,0 +1,9 @@
+import { atlanticNewBrunswickPrograms } from "./atlanticNewBrunswick";
+import { atlanticNovaScotiaPrograms } from "./atlanticNovaScotia";
+import { atlanticPeiNewfoundlandPrograms } from "./atlanticPeiNewfoundland";
+
+export const atlanticPrograms = [
+  ...atlanticNovaScotiaPrograms,
+  ...atlanticNewBrunswickPrograms,
+  ...atlanticPeiNewfoundlandPrograms,
+];
