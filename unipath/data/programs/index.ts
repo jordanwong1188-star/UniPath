f46db4853d89ed20470bf44fedda38df89a1e075
jordanwong1188-star <jordan.wong1188@gmail.com>
2026-08-18 19:50:35 -0700

    Add regional program exports

---
+++ unipath/data/programs/index.ts
@@ -0,0 +1,5 @@
+export { britishColumbiaPrograms } from "./britishColumbia";
+export { ontarioPrograms } from "./ontario";
+export { prairiePrograms } from "./prairies";
+export { quebecAtlanticPrograms } from "./quebecAtlantic";
+export type { ProgramDetail, ProgramSeed } from "./schema";
