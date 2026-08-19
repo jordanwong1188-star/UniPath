4fbb86bbec4847509f42771c9524327227ae8fc3
jordanwong1188-star <jordan.wong1188@gmail.com>
2026-08-18 19:51:37 -0700

    Remove unused program barrel export

--- unipath/data/programs/index.ts
+++
@@ -1,5 +0,0 @@
-export { britishColumbiaPrograms } from "./britishColumbia";
-export { ontarioPrograms } from "./ontario";
-export { prairiePrograms } from "./prairies";
-export { quebecAtlanticPrograms } from "./quebecAtlantic";
-export type { ProgramDetail, ProgramSeed } from "./schema";
