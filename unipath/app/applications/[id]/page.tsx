fd750ef95f340702cdb7eb54e76783e0775ac83d
jordanwong1188-star <jordan.wong1188@gmail.com>
2026-08-20 17:37:25 -0700

    Integrate recorded interview simulator into application workspaces

--- unipath/app/applications/[id]/page.tsx
+++ unipath/app/applications/[id]/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import { useParams } from "next/navigation";
+import VideoInterviewSimulator, { supportsVideoInterview } from "../../components/VideoInterviewSimulator";
 import { ApplicationHub, applicationProfiles } from "../../application-hub/page";
 
 export default function ProgramApplicationPage() {
   const params = useParams<{ id: string }>();
   const id = Array.isArray(params.id) ? params.id[0] : params.id;
-  const exists = applicationProfiles.some(item => item.id === id);
+  const selected = applicationProfiles.find(item => item.id === id) ?? applicationProfiles[0];
 
-  return <ApplicationHub mode="applications" initialApplicationId={exists ? id : applicationProfiles[0].id} showChooser={false} />;
+  return <>
+    {supportsVideoInterview(selected.id) ? <VideoInterviewSimulator profile={selected} /> : null}
+    <ApplicationHub mode="applications" initialApplicationId={selected.id} showChooser={false} />
+  </>;
 }
